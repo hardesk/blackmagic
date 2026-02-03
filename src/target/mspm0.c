567bd65d672f8be0564e368e2f1d536f93345682
hardesk <hardesk17@gmail.com>
2026-02-03 23:45:16 +0200

    mspm0: Flash using on-device stub, support more mspm0 devices

--- src/target/mspm0.c
+++ src/target/mspm0.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of the Black Magic Debug project.
  *
- * Copyright (C) 2024 hardesk <hardesk@gmail.com>
+ * Copyright (C) 2024-2026 hardesk <hardesk17@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,21 +22,19 @@
 #include "target_internal.h"
 #include "buffer_utils.h"
 #include "jep106.h"
-#include "cortex.h"
+#include "cortexm.h"
+
 
 #define MSPM0_CONFIG_FLASH_DUMP_SUPPORT (CONFIG_BMDA == 1 || ENABLE_DEBUG == 1)
 
-#define TI_DEVID_MSPM0C           0xbba1U /* MSPM0C110[34] */
-#define TI_DEVID_MSPM0L           0xbb82U /* MSPM0L110[56], MSPM0L13[04][456] */
-#define TI_DEVID_MSPM0L_1227_2228 0xbb9fU /* MSPM0L[12]22[78]*/
-#define TI_DEVID_MSPM0G           0xbb88U /* MSPM0G310[567], MSPM0G150[567], MSPM0G350[567] */
-
-#define MSPM0_SRAM_BASE       0x20000000U
-#define MSPM0_FLASH_MAIN      0x00000000U
-#define MSPM0_FLASH_NONMAIN   0x41c00000U /* One Sector, BANK0. Device boot configuration (BCR, BSL) */
-#define MSPM0_FLASH_FACTORY   0x41c40000U /* One Sector, BANK0. Non modifiable */
-#define MSPM0_FLASH_DATA      0x41d00000U
-#define MSPM0_FLASH_SECTOR_SZ 1024U
+#define MSPM0_SRAM_BASE             0x20000000U
+#define MSPM0_FLASH_MAIN            0x00000000U
+#define MSPM0_FLASH_NONMAIN         0x41c00000U /* One Sector, BANK0. Device boot configuration (BCR, BSL) */
+#define MSPM0_FLASH_FACTORY         0x41c40000U /* One Sector, BANK0. Non modifiable */
+#define MSPM0_FLASH_DATA            0x41d00000U
+#define MSPM0_FLASH_SECTOR_SZ       1024U
+#define MSPM0_FLASH_WRITE_CHUNK_SZ  MSPM0_FLASH_SECTOR_SZ
+#define MSPM0_FLASH_STUB_STACK_SIZE 0x20U
 
 #define MSPM0_FACTORYREGION_DEVICEID  (MSPM0_FLASH_FACTORY + 0x4U)
 #define MSPM0_FACTORYREGION_SRAMFLASH (MSPM0_FLASH_FACTORY + 0x18U)
@@ -86,10 +84,20 @@
 #define MSPM0_FLASHCTL_STAT_DONE         0x01U
 #define MSPM0_FLASHCTL_STAT_CMDPASS      0x02U
 
+static bool mspm0_flash_erase(target_flash_s *flash, target_addr_t addr, size_t length);
+static bool mspm0_flash_write(target_flash_s *flash, target_addr_t dest, const void *src, size_t length);
+static bool mspm0_mass_erase(target_s *target, platform_timeout_s *print_progess);
+
 typedef struct mspm0_flash {
 	target_flash_s target_flash;
 	uint32_t banks;
+	uint32_t ram_size; /* 0 if stub flashing is disabled */
 } mspm0_flash_s;
+
+static const uint16_t mspm0_flash_write_stub[] = {
+#include "flashstub/mspm0.stub"
+};
+#define STUB_BUFFER_BASE ALIGN(MSPM0_SRAM_BASE + sizeof(mspm0_flash_write_stub), 4)
 
 #if MSPM0_CONFIG_FLASH_DUMP_SUPPORT
 static bool mspm0_dump_factory_config(target_s *target, int argc, const char **argv);
@@ -101,10 +109,6 @@ static command_s mspm0_cmds_list[] = {
 	{NULL, NULL, NULL},
 };
 #endif
-
-static bool mspm0_flash_erase(target_flash_s *flash, target_addr_t addr, size_t length);
-static bool mspm0_flash_write(target_flash_s *flash, target_addr_t dest, const void *src, size_t length);
-static bool mspm0_mass_erase(target_s *target, platform_timeout_s *print_progess);
 
 #if MSPM0_CONFIG_FLASH_DUMP_SUPPORT
 typedef struct conf_register {
@@ -185,7 +189,21 @@ static bool mspm0_dump_bcr_config(target_s *const target, const int argc, const 
 }
 #endif
 
-static void mspm0_add_flash(target_s *const target, const uint32_t base, const size_t length, const uint32_t banks)
+static uint16_t g_mspm0_partnums[] = {
+	0xbba1U, /* MSPM0C: 1103 1104 1103-Q1 1104-Q1 */
+	0x0bbbU, /* MSPM0C: 1105-Q1 1106-Q1 */
+	0xbbbaU, /* MSPM0C: 1105 1106 */
+	0xbb82U, /* MSPM0L: 1105 1106 1304 1305 1305 1344 1345 1346 1345-Q1 1346-Q1 */
+	0xbb9fU, /* MSPM0L: 1227 1228 2227 2228 1227-Q1 1228-Q1 2227-Q1 2228-Q1 */
+	0xbbb4U, /* MSPM0L: 1116 1117 1116-Q1 1117-Q1 */
+	0xbbc7U, /* MSPM0L: 2116 2117 1126 1127 */
+	0x0bbaU, /* MSPM0H: 3215 3216 */
+	0xbb88U, /* MSPM0G: 1105 1106 1107 1505 1506 1507 3105 3106 3107 3505 3506 3507 3105-Q1 3106-Q1 3107-Q1 3505-Q1 3506-Q1 3507-Q1 */
+	0xbba9U, /* MSPM0G: 1518 1519 3518 3519 3518-Q1 3519-Q1 3529-Q1 */
+};
+
+static void mspm0_add_flash(
+	target_s *const target, const uint32_t base, const size_t length, const uint32_t banks, uint32_t sram_size)
 {
 	mspm0_flash_s *const flash = calloc(1, sizeof(*flash));
 	if (flash == NULL) {
@@ -193,12 +211,21 @@ static void mspm0_add_flash(target_s *const target, const uint32_t base, const s
 		return;
 	}
 
+	uint32_t write_size = MSPM0_FLASH_WRITE_CHUNK_SZ;
+	uint32_t stub_plus = ALIGN(sizeof mspm0_flash_write_stub, 4) + MSPM0_FLASH_STUB_STACK_SIZE;
+	if (sram_size >= stub_plus) {
+		uint32_t avail_ram = sram_size - stub_plus;
+		while (write_size > avail_ram)
+			write_size >>= 1;
+		flash->ram_size = sram_size;
+	}
+
 	flash->banks = banks;
 	target_flash_s *target_flash = &flash->target_flash;
 	target_flash->start = base;
 	target_flash->length = length;
 	target_flash->blocksize = MSPM0_FLASH_SECTOR_SZ;
-	target_flash->writesize = 8U;
+	target_flash->writesize = write_size;
 	target_flash->erase = mspm0_flash_erase;
 	target_flash->write = mspm0_flash_write;
 	target_flash->erased = 0xffU;
@@ -214,10 +241,12 @@ bool mspm0_probe(target_s *const target)
 		return false;
 
 	const uint32_t partnum = (deviceid & MSPM0_DEVICEID_PARTNUM_MASK) >> MSPM0_DEVICEID_PARTNUM_SHIFT;
-	if (partnum != TI_DEVID_MSPM0C && partnum != TI_DEVID_MSPM0L && partnum != TI_DEVID_MSPM0L_1227_2228 &&
-		partnum != TI_DEVID_MSPM0G)
-		return false;
+	for (size_t i = 0; i < ARRAY_LENGTH(g_mspm0_partnums); ++i)
+		if (partnum == g_mspm0_partnums[i])
+			goto id_matched;
+	return false;
 
+id_matched:
 	target->driver = "MSPM0";
 	target->target_options |= TOPT_INHIBIT_NRST;
 	target->mass_erase = mspm0_mass_erase;
@@ -235,9 +264,9 @@ bool mspm0_probe(target_s *const target)
 			MSPM0_FACTORYREGION_SRAMFLASH_DATAFLASH_SZ_SHIFT);
 
 	target_add_ram32(target, MSPM0_SRAM_BASE, sram_size);
-	mspm0_add_flash(target, MSPM0_FLASH_MAIN, mainflash_size, main_num_banks);
+	mspm0_add_flash(target, MSPM0_FLASH_MAIN, mainflash_size, main_num_banks, sram_size);
 	if (dataflash_size != 0)
-		mspm0_add_flash(target, MSPM0_FLASH_DATA, dataflash_size, 1U);
+		mspm0_add_flash(target, MSPM0_FLASH_DATA, dataflash_size, 1U, sram_size);
 
 #if MSPM0_CONFIG_FLASH_DUMP_SUPPORT
 	target_add_commands(target, mspm0_cmds_list, "MSPM0");
@@ -278,8 +307,10 @@ static void mspm0_flash_unprotect_sector(target_flash_s *const target_flash, con
 		uint32_t mask = ~(1U << sector);
 		target_mem32_write32(target_flash->t, MSPM0_FLASHCTL_CMDWEPROTA, mask);
 	} else if (sector < 256U) { /* 8 sectors per bit */
-		/* When main flash is single bank, PROTB covers sectors starting after PROTA which is 32k. In multibank case
-		 * PROTB bits overlap PROTA and starts at sector 0. */
+		/* Sectors affected by PROTB depend on the flash configuration. In single-bank
+		* main flash, PROTB applies to sectors after those affected by PROTA
+		* (that is, starting at sector 32). In multi-bank configurations, PROTA overlaps
+		* PROTB, so PROTB applies starting at sector 0.  */
 		uint32_t start_protb_sector = mspm0_flash->banks > 1U ? 0U : 32U;
 		uint32_t mask = ~(1U << ((sector - start_protb_sector) >> 3U));
 		target_mem32_write32(target_flash->t, MSPM0_FLASHCTL_CMDWEPROTB, mask);
@@ -291,9 +322,7 @@ static void mspm0_flash_unprotect_sector(target_flash_s *const target_flash, con
 
 static bool mspm0_flash_erase(target_flash_s *const target_flash, const target_addr_t addr, const size_t length)
 {
-#ifdef DEBUG_TARGET_IS_NOOP
 	(void)length;
-#endif
 
 	target_s *const target = target_flash->t;
 
@@ -316,34 +345,53 @@ static bool mspm0_flash_erase(target_flash_s *const target_flash, const target_a
 static bool mspm0_flash_write(
 	target_flash_s *const target_flash, target_addr_t dest, const void *const src, const size_t length)
 {
-#ifdef DEBUG_TARGET_IS_NOOP
-	(void)length;
-#endif
-
 	target_s *const target = target_flash->t;
+	mspm0_flash_s *flash = (mspm0_flash_s *)target_flash;
 
-	mspm0_flash_unprotect_sector(target_flash, dest);
-	target_mem32_write32(target, MSPM0_FLASHCTL_CMDTYPE, MSPM0_FLASHCTL_CMDTYPE_PROG | MSPM0_FLASHCTL_CMDTYPE_SZ_1WORD);
-	target_mem32_write32(target, MSPM0_FLASHCTL_CMDCTL, 0U);
-	target_mem32_write32(target, MSPM0_FLASHCTL_CMDADDR, dest);
-	target_mem32_write32(target, MSPM0_FLASHCTL_BYTEN, 0xffffffffU);
-	target_mem32_write32(target, MSPM0_FLASHCTL_CMDDATA0, read_le4((const uint8_t *)src, 0U));
-	target_mem32_write32(target, MSPM0_FLASHCTL_CMDDATA1, read_le4((const uint8_t *)src, 4U));
-	target_mem32_write32(target, MSPM0_FLASHCTL_CMDEXEC, MSPM0_FLASHCTL_CMDEXEC_EXEC);
+	DEBUG_TARGET(
+		"%s: Writing flash addr %08" PRIx32 " length %08" PRIx32 "\n", __func__, (uint32_t)dest, (uint32_t)length);
 
-	const uint32_t status = mspm0_flash_wait_done(target);
-	if (!(status & MSPM0_FLASHCTL_STAT_CMDPASS))
-		DEBUG_TARGET("%s: Failed to write to flash, status %08" PRIx32 " addr %08" PRIx32 " length %08" PRIx32 "\n",
-			__func__, status, dest, (uint32_t)length);
+	if (flash->ram_size == 0) {
+		uint32_t status = 0;
+		target_mem32_write32(target, MSPM0_FLASHCTL_CMDCTL, 0U);
+		target_mem32_write32(target, MSPM0_FLASHCTL_BYTEN, 0xffffffffU);
+		target_mem32_write32(
+			target, MSPM0_FLASHCTL_CMDTYPE, MSPM0_FLASHCTL_CMDTYPE_PROG | MSPM0_FLASHCTL_CMDTYPE_SZ_1WORD);
 
-	return status & MSPM0_FLASHCTL_STAT_CMDPASS;
+		for (uint32_t written = 0U; written < length; written += 8) {
+			mspm0_flash_unprotect_sector(target_flash, dest);
+			target_mem32_write32(target, MSPM0_FLASHCTL_CMDADDR, dest + written);
+			target_mem32_write32(target, MSPM0_FLASHCTL_CMDDATA0, read_le4((const uint8_t *)src, written + 0U));
+			target_mem32_write32(target, MSPM0_FLASHCTL_CMDDATA1, read_le4((const uint8_t *)src, written + 4U));
+			target_mem32_write32(target, MSPM0_FLASHCTL_CMDEXEC, MSPM0_FLASHCTL_CMDEXEC_EXEC);
+
+			status = mspm0_flash_wait_done(target);
+			if (!(status & MSPM0_FLASHCTL_STAT_CMDPASS)) {
+				DEBUG_TARGET("%s: Failed to write to flash, status %08" PRIx32 " addr %08" PRIx32 " length %08" PRIx32
+							 "\n",
+					__func__, status, dest, (uint32_t)length);
+				break;
+			}
+		}
+
+		return status & MSPM0_FLASHCTL_STAT_CMDPASS;
+	} else {
+		target_check_error(target);
+		target_mem32_write(target, MSPM0_SRAM_BASE, mspm0_flash_write_stub, sizeof(mspm0_flash_write_stub));
+		target_mem32_write(target, STUB_BUFFER_BASE, src, length);
+		if (target_check_error(target))
+			return false;
+
+		return cortexm_run_stub(
+				   target, MSPM0_SRAM_BASE, dest, STUB_BUFFER_BASE, length, 0, MSPM0_SRAM_BASE + flash->ram_size) == 0;
+	}
 }
 
 static bool mspm0_mass_erase(target_s *const target, platform_timeout_s *const print_progess)
 {
 	bool success = true;
 	for (mspm0_flash_s *flash = (mspm0_flash_s *)target->flash; flash && success;
-		 flash = (mspm0_flash_s *)flash->target_flash.next) {
+		flash = (mspm0_flash_s *)flash->target_flash.next) {
 		/* Assume banks are of same size */
 		uint32_t bank_size = flash->target_flash.length / flash->banks;
 		for (uint32_t bank = 0U; bank < flash->banks; ++bank) {
@@ -360,7 +408,7 @@ static bool mspm0_mass_erase(target_s *const target, platform_timeout_s *const p
 			target_mem32_write32(target, MSPM0_FLASHCTL_CMDEXEC, MSPM0_FLASHCTL_CMDEXEC_EXEC);
 
 			uint32_t status = 0U;
-			while (status & MSPM0_FLASHCTL_STAT_DONE) {
+			while (!(status & MSPM0_FLASHCTL_STAT_DONE)) {
 				status = target_mem32_read32(target, MSPM0_FLASHCTL_STATCMD);
 				if (print_progess)
 					target_print_progress(print_progess);
